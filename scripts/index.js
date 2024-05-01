d9dde78da01da25f151d4fe13bb17d4602275ed5
Manuel Ramos <maplex78@hotmail.com>
2024-04-30 21:01:45 -0500

    se incorpora el homework 2

---
+++ scripts/index.js
@@ -0,0 +1,30 @@
+class Activity {
+    constructor(id, title, description, imgURL) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.imgURL = imgURL;
+    }
+}
+
+class Repository {
+    constructor() {
+        this.activities = [];
+    }
+
+    getAllActivities = () => this.activities;
+
+    createActivity = (id, title, description, imgURL) => {
+        const newActivity = new Activity(id, title, description, imgURL);
+        this.activities.push(newActivity);
+        return newActivity;
+    };
+}
+
+const repository = new Repository();
+
+function addActivity(title, description, imgURL) {
+    const id = Date.now(); // Generar un ID único
+    repository.createActivity(id, title, description, imgURL);
+    displayActivities(); // Actualizar la visualización de las actividades
+}
