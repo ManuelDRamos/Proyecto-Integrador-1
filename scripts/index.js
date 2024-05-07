02658777b09f1d727a0ca2d710c7cc9c7e9f668f
Manuel Ramos <maplex78@hotmail.com>
2024-05-06 21:06:06 -0500

    Proyecto Finalizado

--- scripts/index.js
+++ scripts/index.js
@@ -10,25 +10,92 @@ class Activity {
 class Repository {
     constructor() {
         this.activities = [];
+        this.id = 0;
     }
 
-    getAllActivities = () => this.activities;
-
-    
-    
-    createActivity = (id, title, description, imgURL) => {
-        const newActivity = new Activity(id, title, description, imgURL);
+    addActivity = ({title, description, imgURL}) => {
+        const newActivity = new Activity(this.id, title, description, imgURL);
         this.activities.push(newActivity);
-        return newActivity;
+        this.id++;
     };
+
+
+getActivities() {
+    return this.activities;
 }
 
-
+removeActivity(id) {
+    this.activities = this.activities.filter((activity) => activity.id !== id);
+ }
+};
 
 const repository = new Repository();
 
-function addActivity(title, description, imgURL) {
-    const id = Date.now(); // Generar un ID único
-    repository.createActivity(id, title, description, imgURL);
-    displayActivities(); // Actualizar la visualización de las actividades
-}
+// repositorio.addActivity(
+//     "bailar",
+//     "mover el bote con los pasos prohibidos",
+//     "esta es la imagen"
+// );
+
+repositorio.getActivities();
+
+const button = document.getElementById("addActivity");
+
+const render = () => {
+    const container = document.getElementById("activities-container");
+    container.innerHTML = "";
+
+    const activities = repositorio.getActivities();
+
+    const htmlActivities = activities.map((activity) => {
+        const card = document.createElement("div");
+        card.className = "card";
+
+        const cardImage = document.createElement("img");
+        cardImage.src = activity.imgURL;
+
+        const cardTitle = document.createElement("span");
+        cardTitle.className = "card-title title";
+        cardTitle.innerText = activity.title;
+
+        const cardDescription = document.createElement("p");
+        cardDescription.innerText = activity.description;
+
+        card.appendChild(cardTitle);
+        card.appendChild(cardImage);
+        card.appendChild(cardDescription);
+
+        return card;
+    });
+
+    htmlActivities.map((activity) => {
+        container.appendChild(activity);
+    });
+
+    container.appendChild(...htmlActivities)
+};
+
+
+const addActivityDOM = () => {
+    const title = document.getElementById("title").value;
+    const description = document.getElementById("description").value;
+    const imgURL = document.getElementById("imgUrl").value;
+
+    if (!title || !description || !imgURL) {
+        alert(" Te faltan datos");
+        return;
+    }
+
+    repositorio.addActivity({title,description,imgURL});
+
+    render();
+};
+
+// function addActivity(title, description, imgURL) {
+//     const id = Date.now(); // Generar un ID único
+//     repository.addActivity(id, title, description, imgURL);
+//     displayActivities(); // Actualizar la visualización de las actividades
+// }
+    
+    
+
